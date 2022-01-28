895c414c3086a5d8d295652f94b4db94b044f075
SauravSharma <saurav.sharma@infostride.com>
2022-01-28 13:15:07 +0530

    updated

--- src/main/java/demo/test/Facebook.java
+++ src/main/java/demo/test/Facebook.java
@@ -1,5 +1,7 @@
 package demo.test;
 
+import java.time.Duration;
+
 import org.apache.commons.lang.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,23 +13,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Facebook {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         String firstName = RandomStringUtils.randomAlphabetic(8);
+        String mobileNo = RandomStringUtils.randomNumeric(10);
+        String userPassword = RandomStringUtils.randomAlphanumeric(8);
 
         driver.get("https://www.facebook.com/");
         driver.manage().window().maximize();
         driver.findElement(By.linkText("Create New Account")).click();
-        Thread.sleep(5000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        //second question
         driver.findElement(By.xpath("//button[@class='_6j mvm _6wk _6wl _58mi _3ma _6o _6v']/parent::div[@class='_1lch']"
         		+ "/preceding-sibling::div[@id='fullname_field']//child::input[@name='firstname']")).sendKeys(firstName);
 
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//input[@name='reg_email__']")).sendKeys("8788783939");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        //fourth question
+        driver.findElement(By.xpath("//input[@name='reg_passwd__']//preceding::input[@name='reg_email__']"))
+        .sendKeys(mobileNo);
         
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        //third question
+        driver.findElement(By.xpath("//input[@name='reg_email__']//following::input[@name='reg_passwd__']"))
+        .sendKeys(userPassword);
+        //first question
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         driver.findElement(By.xpath("//div[@class='_1lch']//button[@type='submit']")).click();
-        Thread.sleep(5000);       
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        driver.quit();
 	}
 
 }
