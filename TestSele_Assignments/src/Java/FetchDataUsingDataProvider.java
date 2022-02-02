39198118d3c664d63f8d224935f684c172a0767a
Saurav-infostride <96471360+Saurav-infostride@users.noreply.github.com>
2022-02-02 13:30:14 +0530

    Update FetchDataUsingDataProvider.java

--- TestSele_Assignments/src/Java/FetchDataUsingDataProvider.java
+++ TestSele_Assignments/src/Java/FetchDataUsingDataProvider.java
@@ -23,14 +23,13 @@ public class FetchDataUsingDataProvider {
         sheet =wb.getSheetAt(0);		//To move specific sheet
         int num = sheet.getLastRowNum();
         Object[][] arr= new Object[num][2];
-        System.out.println(num);
         int ci=0;
         for(int i=1;i<=num;i++,ci++) {
             String uname=sheet.getRow(i).getCell(0).getStringCellValue();
             String pass=sheet.getRow(i).getCell(1).getStringCellValue();
 
-            arr[ci][0]=uname;   //to print username
-            arr[ci][1]=pass;	//to print password
+            arr[ci][0]=uname;   //to set username in an arr
+            arr[ci][1]=pass;	//to set password in an arr
         }
 
         return arr;
